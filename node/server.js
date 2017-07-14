98d73612637f87918a5e6cd6abf03e5f0c537fc4
lluu <sureerat@outlook.com>
2017-07-14 23:53:39 +0700

    mounting request path

--- node/server.js
+++ node/server.js
@@ -1,11 +1,27 @@
 var connect = require('connect');
 var app = connect();
 
-app.use(function(req, res, next) {
+// var logger = function(req, res, next) {
+// 	console.log(req.method, req.url);
+
+// 	next();
+// }
+
+var helloWorld = function(req, res, next) {
 //middleware
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Hello World');
-});
+};
+
+
+var goodbyeWorld = function(req, res, next) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Goodbye World');
+};
+
+// app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
 
 app.listen(3000);
 console.log('Server Running at http://localhost:3000');
