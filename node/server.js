a8b084327bea68e856f866aaa0ebe4847b4a13c6
lluu <sureerat@outlook.com>
2017-07-14 23:39:28 +0700

    Hello World w/ connect

--- node/server.js
+++ node/server.js
@@ -1,4 +1,11 @@
 var connect = require('connect');
 var app = connect();
+
+app.use(function(req, res, next) {
+//middleware
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+});
+
 app.listen(3000);
 console.log('Server Running at http://localhost:3000');
